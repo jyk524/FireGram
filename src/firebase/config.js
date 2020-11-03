5f6fe0b22284c157c3f7d3680679af534d7845be
jyk524 <67247717+jyk524@users.noreply.github.com>
2020-11-03 06:49:37 -0500

    first commit

---
+++ src/firebase/config.js
@@ -0,0 +1,21 @@
+import * as firebase from 'firebase/app';
+import 'firebase/storage';
+import 'firebase/firestore';
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyCon2KgAVEuGcemTNJOhh2uRMsu4lCxWZI",
+    authDomain: "jyk-firegram.firebaseapp.com",
+    databaseURL: "https://jyk-firegram.firebaseio.com",
+    projectId: "jyk-firegram",
+    storageBucket: "jyk-firegram.appspot.com",
+    messagingSenderId: "1075349596769",
+    appId: "1:1075349596769:web:eb60695ee6a8c556929820"
+  };
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const projectStorage = firebase.storage();
+const projectFirestore = firebase.firestore();
+
+export { projectStorage, projectFirestore };
