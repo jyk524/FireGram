f2ad6e886d4ad1892aa7807bcdf5d1593ebe4c82
jyk524 <67247717+jyk524@users.noreply.github.com>
2020-11-05 07:08:11 -0500

    added upload form styling with label and changed to +

--- src/comps/Modal.js
+++ src/comps/Modal.js
@@ -15,7 +15,10 @@ const Modal = ({ selectedImg, setSelectedImg }) => {
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
     >
-        <img src={selectedImg} alt="enlarged pic" />
+        <motion.img src={selectedImg} alt="enlarged pic" 
+            initial={{ y: "-100vh" }}
+            animate={{ y: 0 }}
+        />
     </motion.div>
   )
 
